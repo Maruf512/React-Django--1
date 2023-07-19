08d2338126492313de5bd32cabfac632cd4cb133
Maruf Ahmed <marufsarkar512@gmail.com>
2023-07-19 09:58:41 +0600

    making adjustments

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 def main(request):
     return HttpResponse("<h1>Hello</h1>")
+
+
+# login page
+def login(request):
+    return HttpResponse("<h2>Welcome to the Login page.</h2>")
+
+
